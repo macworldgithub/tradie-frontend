13aca08aaa10fd14fa1a96cb63e6fcf16f942c7b
Ayesha Fahim <fahim.ayesha000@gmail.com>
2026-05-24 23:12:52 -0700

    out of hours done

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -354,7 +354,7 @@ export default function Signup({ onBack, onSuccess }: SignupProps) {
               </p>
             </div>
 
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+            {/* <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
               <InputField
                 label="Opening Time"
                 value={formData.openingTime}
@@ -369,7 +369,7 @@ export default function Signup({ onBack, onSuccess }: SignupProps) {
                 placeholder="06:00 PM"
                 onChange={(v: string) => handleInputChange("closingTime", v)}
               />
-            </div>
+            </div> */}
 
             <InputField
               label="Secondary SMS Delivery (optional)"
