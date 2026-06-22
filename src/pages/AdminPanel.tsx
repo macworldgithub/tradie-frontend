0ef4beef01244b8579757b7e1d62ce7e580d7005
ayla-imran851 <ayla@macworldtechnology.com>
2026-06-22 16:05:56 +0500

    issues resolved

--- src/pages/AdminPanel.tsx
+++ src/pages/AdminPanel.tsx
@@ -665,7 +665,7 @@ export default function AdminPanel({
                       </div>
                     ) : (
                       /* Card Grid */
-                      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+                      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-6">
                         {filteredCompanies.map((company) => (
                           <div
                             key={company.companyId}
